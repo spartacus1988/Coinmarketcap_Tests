f1693903f743ef8570aa0ef806c3708c24cb3873
spartacusxxxxxx <spartacusxxxxxx@gmail.com>
2019-06-18 17:57:40 +0300

    Pool was added

--- main.py
+++ main.py
@@ -3,22 +3,24 @@ import json as jsn
 import time
 import sys
 from datetime import datetime
+from multiprocessing import Pool
+#import multiprocessing
 
 class CoinmarketcapSpider:
 
 	def __init__(self):
-	 	self.json = None
-	 	self.start_time = None
-	 	self.delta_time = None
-	 	self.sizeofjson = None
-	 	self.list_ids = list()
-	 	self.list_timestamps = list()
-	 	self.coinmarketcap = Market()
+		self.json = None
+		self.start_time = None
+		self.delta_time = None
+		self.sizeofjson = None
+		self.list_ids = list()
+		self.list_timestamps = list()
+		self.coinmarketcap = Market()
 
 	def get_first_10_ticker_by_rank(self):
 		
 		self.start_time = time.time()
-		self.json = self.coinmarketcap.ticker(start=0, limit=3, convert='USD', sort='rank')
+		self.json = self.coinmarketcap.ticker(start=0, limit=10, convert='USD', sort='rank')
 		self.delta_time = time.time() - self.start_time
 		self.sizeofjson = sys.getsizeof(self.json)
 
@@ -44,29 +46,39 @@ class CoinmarketcapSpider:
 
 
 
-	def first_test_case(self):
-		self.get_first_10_ticker_by_rank()
-		assert self.delta_time < 0.5
+def first_test_case(CoinmarketcapSpider):
+	CoinmarketcapSpider.get_first_10_ticker_by_rank()
+	assert CoinmarketcapSpider.delta_time < 0.5
 
-		self.fill_list_timestamps()
-		for curts in self.list_timestamps:
-			print(datetime.utcfromtimestamp(curts).strftime('%Y-%m-%d'))
-			curtime = datetime.utcfromtimestamp(time.time()).strftime('%Y-%m-%d')
-			print("curtime_is:" + curtime)
-			assert datetime.utcfromtimestamp(curts).strftime('%Y-%m-%d') == curtime
+	CoinmarketcapSpider.fill_list_timestamps()
+	for curts in CoinmarketcapSpider.list_timestamps:
+		print(datetime.utcfromtimestamp(curts).strftime('%Y-%m-%d'))
+		curtime = datetime.utcfromtimestamp(time.time()).strftime('%Y-%m-%d')
+		print("curtime_is:" + curtime)
+		assert datetime.utcfromtimestamp(curts).strftime('%Y-%m-%d') == curtime
 
-		assert self.sizeofjson < 10240
+	assert CoinmarketcapSpider.sizeofjson < 10240
+
+	return CoinmarketcapSpider.delta_time
 			
-		
+
 	
-			
-
-
-
-
 
 if __name__ == "__main__":
 
-	CoinmarketcapSpider = CoinmarketcapSpider()
-	#CoinmarketcapSpider.get_first_10_ticker_by_rank()
-	CoinmarketcapSpider.first_test_case()
+	CoinmarketcapSpiderInstance = CoinmarketcapSpider()
+	#CoinmarketcapSpiderInstanceII = CoinmarketcapSpider()
+	first_test_case(CoinmarketcapSpiderInstance)
+
+	itterable_list = []
+	for i in range(8):
+		CoinmarketcapSpiderInstanceII = CoinmarketcapSpider()
+		itterable_list.append(CoinmarketcapSpiderInstanceII)
+
+
+	results = []
+	with Pool(2) as pool:
+		pool.map(first_test_case, itterable_list)
+
+
+
